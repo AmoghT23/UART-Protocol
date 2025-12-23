7eb1c74c70c2fd56487296ba5e16dcf2289bf46c
Amogh Vinod Thakur <90680979+AmoghT23@users.noreply.github.com>
2025-12-23 11:27:16 -0800

    Update UART_tx.sv

--- UART_tx.sv
+++ UART_tx.sv
@@ -1,64 +1,73 @@
-module transmitter(input logic clk, rstn,  
-                   input logic [7:0] data_in,
-                   input logic tx_enb,
-                   input logic wr_enb,
-                   output logic tx,
-                   output logic busy);
+module transmitter (
+  uart_if.uart1 intf,
+  input logic tx_enb
+);
 
-typedef enum logic [1:0] {IDLE, START, DATA, STOP} state_tx;
-state_tx current_state, next_state;
+  typedef enum logic [1:0] {IDLE, START, DATA, STOP} state_tx;
+  state_tx current_state, next_state;
 
   logic [2:0] bit_counter;
   logic [7:0] shift_reg;
 
-  always @(posedge clk) begin
-    if (!rstn) 
+  always_ff @(posedge intf.clk or posedge intf.rst) begin
+    if (intf.rst)
       current_state <= IDLE;
-    else if(tx_enb)
+    else if (tx_enb)
       current_state <= next_state;
   end
 
-  always_ff@ (posedge clk or negedge rstn) begin
-    if(!rstn) begin
-      bit_counter = 3'd0;
-      shift_reg = 8'd0;
-    end 
-    
-    else if(current_state == IDLE && wr_enb)begin
-    shift_reg <= data_in;
-    bit_counter <= 3'd0;
+  always_ff @(posedge intf.clk or posedge intf.rst) begin
+    if (intf.rst) begin
+      bit_counter <= 3'd0;
+      shift_reg   <= 8'd0;
     end
-    
-    else if(current_state == DATA && tx_enb) begin
+    else if (current_state == IDLE && intf.newd) begin
+      shift_reg   <= intf.dintx;
+      bit_counter <= 3'd0;
+    end
+    else if (current_state == DATA && tx_enb) begin
+      shift_reg   <= shift_reg >> 1;
       bit_counter <= bit_counter + 1'b1;
-      shift_reg <= shift_reg >> 1;
-    end 
+    end
   end
-  
+
   always_comb begin
-    case (current_state) 
-      IDLE: if(wr_enb) 
-        next_state <= START;
-      
-      START: if(tx_enb)
-        next_state <= DATA;
+    next_state = current_state;
 
-      DATA: if(tx_enb && bit_counter == 3'd7)
-          next_state <= STOP;
+    case (current_state)
+      IDLE:
+        if (intf.newd)
+          next_state = START;
 
-      STOP: if(tx_enb)
-          next_state <= IDLE;
+      START:
+        if (tx_enb)
+          next_state = DATA;
+
+      DATA:
+        if (tx_enb && bit_counter == 3'd7)
+          next_state = STOP;
+
+      STOP:
+        if (tx_enb)
+          next_state = IDLE;
+
+      default:
+        next_state = IDLE;
     endcase
   end
 
-    always_comb begin
-      tx = 1'b1;
-      case (current_state)
-        START: tx = 1'b0;
-        DATA: tx = shift_reg[0];
-        STOP: tx = 1'b1;
-      endcase
-    end
-    
-  assign busy = (current_state != IDLE);
-    endmodule 
+  always_comb begin
+    intf.tx     = 1'b1;
+    intf.donetx = 1'b0;
+
+    case (current_state)
+      START: intf.tx = 1'b0;
+      DATA:  intf.tx = shift_reg[0];
+      STOP: begin
+        intf.tx     = 1'b1;
+        intf.donetx = 1'b1;
+      end
+    endcase
+  end
+
+endmodule
